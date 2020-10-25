9bc2bc9cb7dbec882172048c2598f0fe713a7dee
developer-onizuka <his.onioni@gmail.com>
2020-10-25 16:01:31 +0900

    2020-10-25

---
+++ Calculation/Mathlib/Add.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Caluclation.Mathlib
+{
+	public class CalculationAdd
+	{
+		public int Add(int a, int b)
+		{
+			return a + b;
+		}
+	}
+}
+
---
+++ Calculation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Caluclation.Mathlib;
+
+namespace Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    CalculationAdd val = new CalculationAdd();
+            Console.WriteLine(val.Add(1,2));
+        }
+    }
+}
