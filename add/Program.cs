707140af909bc0872ffeacff33034fdc4085cbb0
developer-onizuka <his.onioni@gmail.com>
2020-10-25 14:40:53 +0900

    2020-10-25

---
+++ add/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace add
+{
+    class Program
+    {
+        public static int Add(int a, int b)
+        {
+    	    return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+	    Console.WriteLine(Add(1,2));
+        }
+    }
+}
